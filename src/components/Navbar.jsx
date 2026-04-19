7149906de3880f6c488289877fef1eba5ed44fc8
Dhruv-Datta <dattadhruv930@gmail.com>
2026-04-18 22:03:53 -0500

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,7 +152,7 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
+      className={`fixed top-0 left-0 right-0 z-[9999] transition-all duration-500 ${
         isDark
           ? scrolled ? 'shadow-lg shadow-black/20' : ''
           : scrolled ? 'shadow-lg shadow-gray-200/50' : ''
